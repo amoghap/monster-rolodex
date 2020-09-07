4ca64eb5e3906cc86fe470d51f6f16e37b945659
Amogh Pradhan <amopradhan@gmail.com>
2020-09-07 18:35:07 -0400

    Updates

---
+++ precache-manifest.fc3a3fb5c79e41980d5586180e9537f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd55bca74da51bfb8a405c5c113c8c78",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2e7dc7a3761e2d37860f",
+    "url": "/monster-rolodex/static/css/main.4e398d8d.chunk.css"
+  },
+  {
+    "revision": "79ad37e630052ae7a401",
+    "url": "/monster-rolodex/static/js/2.2c32a551.chunk.js"
+  },
+  {
+    "revision": "2e7dc7a3761e2d37860f",
+    "url": "/monster-rolodex/static/js/main.81ea7930.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
